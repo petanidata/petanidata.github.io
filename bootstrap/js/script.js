66ce7e6fb064525267ff1d14b828fea688aa1e3c
Rima <43285144+rimanurfah@users.noreply.github.com>
2020-06-04 06:03:36 +0700

    Upload file

---
+++ bootstrap/js/script.js
@@ -0,0 +1,43 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+function HargaLogistic(){
+  var harga;
+  var x = document.getElementById("ongkirku")
+  if (x="one"){
+    harga = "100";
+  } else if (x="two"){
+    harga = "200";
+  } else{
+    harga = "300";
+  }
+
+  document.getElementById("hargaOngkir").innerHTML = harga;
+}
+
